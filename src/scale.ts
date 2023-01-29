340651c7222afeac2953dd2a630a98476a1e460b
Chocolateandmilkwin <52867415+Chocolateandmilkwin@users.noreply.github.com>
2023-01-29 10:39:07 +0300

    Scale is not applied to document after reload (#9)

--- src/scale.ts
+++ src/scale.ts
@@ -4,9 +4,10 @@ import { events, forDocuments } from "@chocolatelibui/document"
 //Package exports
 /** Ui scale */
 export let scale = settings.makeNumberSetting('scale', 'UI Scale', 'The scale of the UI', 1, 0.2, 4);
+console.log(scale.get);
 
 //Adds listener for scale change
-let actualScale = 16;
+let actualScale = <number>scale.get * 16;
 scale.addListener((val) => {
     actualScale = val * 16;
     forDocuments((doc) => {
